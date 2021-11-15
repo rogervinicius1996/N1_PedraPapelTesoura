fbf228853ded89630a19ce2fced72b0e02add7f3
Roger Vinicius <rogervinicius1996@gmail.com>
2021-11-15 15:46:58 -0300

    Feat : Adiciona arquivos projeto N1
    Aquivos adicionados:
    	   .gitignore
    	   README.md
    	   bin/App.class
    	   bin/Jogo.class
    	   src/App.java
    	   src/Jogo.java

---
+++ src/Jogo.java
@@ -0,0 +1,19 @@
+public class Jogo {
+    public String resultado(int jogador, int pc) {
+        if (jogador == pc) {
+            return "EMPATOU - jogue novamente";
+        } else if (jogador == 1 && pc == 2) {
+            return "Pedra(você) quebra Tesoura(pc) --Você ganhou!!";
+        } else if (jogador == 1 && pc == 3) {
+            return "Papel(pc) embrulha Pedra(você) -- Você perdeu!!";
+        } else if (jogador == 2 && pc == 1) {
+            return "Pedra(pc) quebra Tesoura(você) -- Você perdeu !!";
+        } else if (jogador == 2 && pc == 3) {
+            return "Tesoura(você) corta Papel(pc) -- Você ganhou!!";
+        } else if (jogador == 3 && pc == 1) {
+            return "Papel(você) embrulha Pedra(pc) -- Você ganhou!!";
+        } else {
+            return "Tesoura(pc) corta Papel(você) -- Você perdeu!!";
+        }
+    }
+}
